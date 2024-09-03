15e56a211fabc0b020def78baba9d9b6bce78c6f
DenysHarkavenko <garkavenkodenispost@gmail.com>
2024-09-03 18:11:52 +0200

    create first section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import Image from "next/image";
+
+import CreatingJourneys from "@/app/components/CreatingJourneys";
+import StayAhead from "@/app/components/StayAhead";
+
+
+
 
 export default function Home() {
-  return (
-    <main className="">
-      <header className='px-20 py-3 relative'>
-          <div className='absolute w-full '>
 
-          </div>
-      </header>
-    </main>
-  );
+    return (
+        <>
+            <header className="px-20 py-3 relative">
+                <div className="absolute w-full "></div>
+            </header>
+            <main className="bg-beige h-12 w-full flex-1">
+                <CreatingJourneys/>
+                <StayAhead/>
+            </main>
+            <footer></footer>
+        </>
+    );
 }
