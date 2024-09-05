b433631693ac7a0779f6a87ba0dcf3e33aa96dcf
DenysHarkavenko <garkavenkodenispost@gmail.com>
2024-09-05 13:50:18 +0200

    complete footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CreatingJourneys from "@/app/components/CreatingJourneys";
 import StayAhead from "@/app/components/StayAhead";
 import JoinUs from "@/app/components/JoinUs";
+// import Image from "next/image";
 
 
 
@@ -9,10 +10,12 @@ export default function Home() {
 
     return (
         <>
-            <header className="px-20 py-3 relative">
-                <div className="absolute w-full "></div>
+            <header className="px-20 relative bg-beige">
+                {/*<div className="absolute w-full ">*/}
+                {/*    <Image src={'/img/main/hero.png'} alt={'Hero img'} width={1440} height={766} className={'w-full h-auto'}/>*/}
+                {/*</div>*/}
             </header>
-            <main className="bg-beige h-12 w-full flex-1">
+            <main className="bg-beige h-12 w-full flex-1 ">
                 <CreatingJourneys/>
                 <StayAhead/>
                 <JoinUs/>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from "next/image";
 import Box from "@/components/Box";
-// import Link from "next/link";
+import Link from "next/link";
 
 const Footer = () => {
     return (
@@ -10,10 +10,33 @@ const Footer = () => {
                 <div className='flex flex-col gap-14 items-center'>
                     <Image src={'/img/icons/small-logo.svg'} alt='logo' width={43} height={50}/>
                     <p className='uppercase text-xl'>Follow us social media</p>
-                    <nav className='flex-6'>
-                        {/*<Link href={}></Link>*/}
-                        {/*<Link href={}></Link>*/}
-
+                    <nav className='flex gap-6'>
+                        <Link href={'#'}>
+                            <Image src={'/img/icons/facebook.svg'} width={30} height={30} alt={'Facebook'} />
+                        </Link>
+                        <Link href={'#'}>
+                            <Image src={'/img/icons/tik-tok.svg'} width={30} height={30} alt={'TikTok'} />
+                        </Link>
+                        <Link href={'#'}>
+                            <Image src={'/img/icons/instagram.svg'} width={30} height={30} alt={'Instagram'} />
+                        </Link>
+                        <Link href={'#'}>
+                            <Image src={'/img/icons/x.svg'} width={30} height={30} alt={'X'} />
+                        </Link>
+                    </nav>
+                    <nav className='flex gap-6 text-xs'>
+                        <Link href={'mailto:info@encoretix.com'} className='underline'>
+                            info@encoretix.com
+                        </Link>
+                        <Link href={'tel:3051252288'} className='underline'>
+                            (305)&#32;125&#32;22&#32;88
+                        </Link>
+                        <Link href={'#'}>
+                            Terms of services
+                        </Link>
+                        <Link href={'#'}>
+                            Privacy Policy
+                        </Link>
                     </nav>
                 </div>
             </Box>
