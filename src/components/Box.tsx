c4c60c95b5d1c68ddceb3738c7d1c973ede700bf
DenysHarkavenko <garkavenkodenispost@gmail.com>
2024-09-10 13:48:20 +0200

    fix: Box padding-y

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from "react";
 
 const Box = ({children, className}:{children: ReactNode, className?: string }) => {
     return (
-        <div className={`max-w-[1440px] mx-auto py-12 px:py-20 px-4 lg:px-20 ${className}`}>{children}</div>
+        <div className={`max-w-[1440px] mx-auto py-12 md:py-20 px-4 lg:px-20 ${className}`}>{children}</div>
     )
 }
 
