d84956fee6a5cbe2e04bfed980f0416aa7452ca8
DenysHarkavenko <garkavenkodenispost@gmail.com>
2024-09-10 11:59:52 +0200

    feet: create hero section

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from "next/image";
+
+const Hero = () => {
+    return (
+        <div className='relative'>
+            <Image src={'/img/main/hero.png'} alt={'Hero img'} width={1440} height={766}
+                   className={'w-full h-[96vh]'}/>
+
+            <div className='absolute inset-0 flex flex-col items-center justify-center -tracking-tight'>
+                <h1 className='text-white font-black text-4xl italic uppercase'>Your Access to Elevating
+                    Experiences</h1>
+                <button className='py-5 px-6 mt-20 bg-white text-black mix-blend-difference uppercase'>
+                    Contact Us
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,18 @@
 import CreatingJourneys from "@/app/components/CreatingJourneys";
 import StayAhead from "@/app/components/StayAhead";
 import JoinUs from "@/app/components/JoinUs";
-import Image from "next/image";
 import React from "react";
+import Hero from "@/app/components/Hero";
 
 
 export default function Home() {
 
     return (
-        <>
-
-            <main className="bg-beige w-full flex-1 ">
-                <div>
-                    <Image src={'/img/main/hero.png'} alt={'Hero img'} width={1440} height={766}
-                           className={'w-full h-[96vh]'}/>
-                </div>
-                <CreatingJourneys/>
-                <StayAhead/>
-                <JoinUs/>
-            </main>
-        </>
+        <main className="bg-beige w-full flex-1 ">
+            <Hero/>
+            <CreatingJourneys/>
+            <StayAhead/>
+            <JoinUs/>
+        </main>
     );
 }
