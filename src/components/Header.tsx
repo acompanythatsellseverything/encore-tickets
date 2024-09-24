9c2053c76c77e9641700b7bdb6564e8856a81825
nechamdod <nechamdod@gmail.com>
2024-09-24 14:28:47 +0300

    fix: burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import Link from "next/link";
 import Image from "next/image";
-import {X} from "lucide-react";
 import {navLinks} from "@/data/navLinks";
 import useWindowInnerWidth from '@/hooks/useWindowInnerWidth';
+import { motion } from 'framer-motion';
 
 const Header = () => {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -35,6 +35,14 @@ const Header = () => {
     const toggleModal = () => {
         setIsModalOpen(prevState => !prevState);
     };
+
+    useEffect(() => {
+        if (isModalOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    }, [isModalOpen])
 
     return (
         <>
@@ -108,46 +116,62 @@ const Header = () => {
 
             {/* Modal Menu */}
             {isModalOpen && (
-                <div className="fixed top-0 right-0 w-full h-full px-3 py-2 md:p-0 md:h-fit md:mr-20 md:mt-2  z-50 flex justify-end">
-                    <div className="bg-secondary h-full px-10 md:px-20 py-14 max-w-[480px] w-full text-white bg-opacity-90 md:bg-opacity-100">
-                        <button onClick={toggleModal} className="text-white text-xl mb-4 absolute right-4 top-4"><X/></button>
-                        <nav className="flex flex-col gap-[60px] md:gap-[70px]">
-                            {navLinks.map((link, i) => (
-                                <Link href={link.link} onClick={toggleModal} className='uppercase text-sm md:text-xl' key={i}>{link.label}</Link>
-                            ))}
-                            <div className='flex flex-col gap-14 text-xs'>
-                                <Link onClick={toggleModal} href={'mailto:info@encoretix.com'} className='underline'>
-                                    info@encoretix.com
-                                </Link>
-                                <Link onClick={toggleModal} href={'tel:3051252288'} className='underline'>
-                                    (305)&#32;125&#32;22&#32;88
-                                </Link>
-                            </div>
-                            <div className='flex flex-col gap-9 text-xs'>
-                                <Link onClick={toggleModal} href={'/terms-of-service'}>
-                                    Terms of services
-                                </Link>
-                                <Link onClick={toggleModal} href={'/privacy-policy'}>
-                                    Privacy Policy
-                                </Link>
-                            </div>
-                            <div className='flex gap-6'>
-                                <Link href={'#'}>
-                                    <Image src={'/img/icons/facebook-white.svg'} width={30} height={30} alt={'Facebook'} />
-                                </Link>
-                                <Link href={'#'}>
-                                    <Image src={'/img/icons/tik-tok-white.svg'} width={30} height={30} alt={'TikTok'} />
-                                </Link>
-                                <Link href={'#'}>
-                                    <Image src={'/img/icons/inst-white.svg'} width={30} height={30} alt={'Instagram'} />
-                                </Link>
-                                <Link href={'#'}>
-                                    <Image src={'/img/icons/x-white.svg'} width={30} height={30} alt={'X'} />
-                                </Link>
-                            </div>
-                        </nav>
+                <>
+                    <div className="fixed top-0 right-0 w-full h-full px-2 py-2 md:p-0 md:h-fit md:mr-16 md:mt-2  z-50 flex justify-end">
+                        <div className="bg-secondary h-full px-10 md:px-20 py-14 max-w-[480px] w-full text-white bg-opacity-90 md:bg-opacity-100">
+                            <button onClick={toggleModal} className="text-white text-xl absolute right-4 top-6 w-[35px] h-[16px]">
+                                <motion.span
+                                    className="block w-9 h-0.5 bg-white absolute"
+                                    initial={{ rotate: 80, y: 0 }}
+                                    animate={{ rotate: 160, y: 0 }}
+                                    transition={{ duration: 0.3 }}
+                                    />
+                                <motion.span
+                                    className="block w-9 h-0.5 bg-white absolute"
+                                    initial={{ rotate: -80, y: 0 }}
+                                    animate={{ rotate: -160, y: 0 }}
+                                    transition={{ duration: 0.3 }}
+                                    />
+                            </button>
+                            <nav className="flex flex-col gap-[50px] md:gap-[70px]">
+                                {navLinks.map((link, i) => (
+                                    <Link href={link.link} onClick={toggleModal} className='uppercase text-sm md:text-xl' key={i}>{link.label}</Link>
+                                ))}
+                                <div className='flex flex-col gap-14 text-xs'>
+                                    <Link onClick={toggleModal} href={'mailto:info@encoretix.com'} className='underline'>
+                                        info@encoretix.com
+                                    </Link>
+                                    <Link onClick={toggleModal} href={'tel:3051252288'} className='underline'>
+                                        (305)&#32;125&#32;22&#32;88
+                                    </Link>
+                                </div>
+                                <div className='flex flex-col gap-9 text-xs'>
+                                    <Link onClick={toggleModal} href={'/terms-of-service'}>
+                                        Terms of services
+                                    </Link>
+                                    <Link onClick={toggleModal} href={'/privacy-policy'}>
+                                        Privacy Policy
+                                    </Link>
+                                </div>
+                                <div className='flex gap-6'>
+                                    <Link href={'#'}>
+                                        <Image src={'/img/icons/facebook-white.svg'} width={30} height={30} alt={'Facebook'} />
+                                    </Link>
+                                    <Link href={'#'}>
+                                        <Image src={'/img/icons/tik-tok-white.svg'} width={30} height={30} alt={'TikTok'} />
+                                    </Link>
+                                    <Link href={'#'}>
+                                        <Image src={'/img/icons/inst-white.svg'} width={30} height={30} alt={'Instagram'} />
+                                    </Link>
+                                    <Link href={'#'}>
+                                        <Image src={'/img/icons/x-white.svg'} width={30} height={30} alt={'X'} />
+                                    </Link>
+                                </div>
+                            </nav>
+                        </div>
                     </div>
-                </div>
+                    <div className='absolute h-full w-full z-40 bg-secondary bg-opacity-50'></div>
+                </>
             )}
         </>
     );
