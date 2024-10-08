e23525797015394f1e51f3d149a160196883ab4a
DenysHarkavenko <garkavenkodenispost@gmail.com>
2024-10-08 18:34:17 +0200

    upd: update hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
 import Image from 'next/image';
+import ContactModal from "@/components/ContactModal";
 
 interface IProps {
 	title?: string;
@@ -7,10 +9,12 @@ interface IProps {
 	element?: any
 }
 const Hero = (props: IProps) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
 	return (
-		<div className='relative'>
-			{props.element && (
-				(props.element.cover.ext === '.mp4' || props.element.cover.ext === '.mpeg' || props.element.cover.ext === '.quicktime' || props.element.cover.ext === '.wmv' || props.element.cover.ext === '.avi'  || props.element.cover.ext === '.flv' ) ?
+		<>
+			<div className='relative'>
+				{props.element && (
+					(props.element.cover.ext === '.mp4' || props.element.cover.ext === '.mpeg' || props.element.cover.ext === '.quicktime' || props.element.cover.ext === '.wmv' || props.element.cover.ext === '.avi' || props.element.cover.ext === '.flv') ?
 						<div>
 							<video
 								width="100%"
@@ -32,36 +36,49 @@ const Hero = (props: IProps) => {
 							className={'w-full h-[600px] md:h-screen'}
 						/>
 
-			) }
-			{!props.element && (
-				<div>
-					<video
-						width="100%"
-						height="600px"
-						autoPlay
-						muted
-						loop
-						className="h-[600px] md:h-screen object-cover"
+				)}
+				{!props.element && (
+					<div>
+						<video
+							width="100%"
+							height="600px"
+							autoPlay
+							muted
+							loop
+							className="h-[600px] md:h-screen object-cover"
+						>
+							<source src="/vid/hero.mp4" type="video/mp4"/>
+							Your browser does not support the video tag.
+						</video>
+					</div>
+				)}
+				<div className='absolute inset-0 flex flex-col items-center justify-center -tracking-tight'>
+					<div className='flex w-full flex-col gap-2'>
+						<h1 className='text-white font-black text-2xl md:text-4xl italic uppercase text-center'>
+							{props.title ? props.title : "Your Access to Elevating Experiences"}
+						</h1>
+						{props.element && <p>{props.element.city}</p>}
+						{props.element && <p>{props.element.date}</p>}
+					</div>
+
+					<button
+						className='py-4 px-6 mt-20 bg-white text-sm md:text-base text-secondary mix-blend-difference uppercase'
+						onClick={() => setIsModalOpen(true)}
 					>
-						<source src="/vid/hero.mp4" type="video/mp4"/>
-						Your browser does not support the video tag.
-					</video>
+						Contact Us
+					</button>
 				</div>
-			)}
-			<div className='absolute inset-0 flex flex-col items-center justify-center -tracking-tight'>
-				<h1 className='text-white font-black text-2xl md:text-4xl italic uppercase text-center'>
-					{props.title ? props.title : "Your Access to Elevating Experiences"}
-				</h1>
-				<button
-					className='py-4 px-6 mt-20 bg-white text-sm md:text-base text-secondary mix-blend-difference uppercase'>
-					Contact Us
-				</button>
+				<div
+					className='absolute bg-beige bottom-0 z-10 h-16 md:h-48 w-full rounded-t-[40px_40px_0_0] [clip-path:ellipse(52%_30px_at_50%_100%)] md:[clip-path:ellipse(52%_80px_at_50%_100%)]'
+				>
+				</div>
+				<ContactModal
+					isOpen={isModalOpen}
+					onClose={() => setIsModalOpen(false)}
+				/>
 			</div>
-			<div
-				className='absolute bg-beige bottom-0 z-10 h-16 md:h-48 w-full rounded-t-[40px_40px_0_0] [clip-path:ellipse(52%_30px_at_50%_100%)] md:[clip-path:ellipse(52%_80px_at_50%_100%)]'
-			>
-			</div>
-		</div>
+		</>
+
 	);
 };
 
