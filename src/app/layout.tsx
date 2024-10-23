11fd06d6daf1d2597659d396bd3b029432bbabeb
DenysHarkavenko <garkavenkodenispost@gmail.com>
2024-10-23 13:27:37 +0200

    feat: add favicon & favicon SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,29 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import dynamic from "next/dynamic";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    icons: {
+        icon: '/favicon.png',
+    },
+    openGraph: {
+        images: [
+            {
+                url: '/favicon.png',
+                alt: 'Encore Tickets Favicon',
+            },
+        ],
+    },
+    twitter: {
+        images: [
+            {
+                url: '/favicon.png',
+                alt: 'Encore Tickets Favicon',
+            },
+        ],
+    },
+};
 
 
 const inter = Inter({ subsets: ["latin"] })
@@ -15,14 +38,14 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-
-      <script src="//code.tidio.co/lu6cigzk5m2anspiwcytnd8zthsifbiu.js" async></script>
+        <link rel="icon" href="/favicon.png"/>
+        <script src="//code.tidio.co/lu6cigzk5m2anspiwcytnd8zthsifbiu.js" async></script>
     </head>
     <body className={inter.className + ' flex flex-col relative'}>
-    <Header/>
-    {children}
-    <Footer/>
-    </body>
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   );
 }
